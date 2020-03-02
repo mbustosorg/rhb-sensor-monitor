23e38b884f1287eb22da678dba647f4049a07f57
Mauricio Bustos <m@bustos.org>
2020-03-01 16:15:57 -0800

    test cases

--- pressure_sensor.py
+++ pressure_sensor.py
@@ -1,20 +1,33 @@
+"""
+    Copyright (C) 2020 Mauricio Bustos (m@bustos.org)
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import smbus
 import time
 
-# Get I2C bus
 bus = smbus.SMBus(1)
 
 # ADS1115 address, 0x48(72)
 # Select configuration register, 0x01(01)
 #		0x8483(33923)	AINP = AIN0 and AINN = AIN1, +/- 2.048V
 #				Continuous conversion mode, 128SPS
+
 data = [0x84,0x83]
 bus.write_i2c_block_data(0x48, 0x01, data)
 time.sleep(0.5)
 
 def read_pressure():
-    global last_data
-    
+    """ Read pressure sensor from ADC """
     try:
         raw_adc = 0
         data = bus.read_i2c_block_data(0x48, 0x00, 2)
